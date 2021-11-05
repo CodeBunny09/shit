c0e358d215f9e5d418b606b7f0d249c576f4e399
Pratik Choudhuri <pratikchoudhuri1609@gmail.com>
2021-11-05 21:00:39 +0530

    Made the frontend for posts

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,6 @@ def index(req):
     homePageData = HomePageData.objects.all()[0]
     context = {'data': homePageData}
     return render(req, 'blog/index.html', context)
+
+def posts(req):
+    return render(req, 'blog/posts.html')
